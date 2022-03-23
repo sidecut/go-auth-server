d9d8c70aa9bfc0a6efa0f1ad2d39db1be91e411c
James Raden <james.raden@gmail.com>
2022-03-23 14:32:47 -0400

    Add sample code from medium article https://medium.com/golangd/how-to-create-an-authentication-api-with-golang-cf387f020925

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func GetMethod(c *gin.Context) {
+	fmt.Println("\n'GetMethod' called")
+	IdValue := c.Params.ByName("IdValue")
+	message := "GetMethod Called With Param: " + IdValue
+	c.JSON(http.StatusOK, message)
+	ReqPayload := make([]byte, 1024)
+	ReqPayload, err := c.GetRawData()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Request Payload Data: ", string(ReqPayload))
+}
+
+func main() {
+	router := gin.Default()
+	subRouterAuthenticated := router.Group("/api/v1/PersonId", gin.BasicAuth(gin.Accounts{
+		"admin": "adminpass",
+	}))
+	subRouterAuthenticated.GET("/:IdValue", GetMethod)
+	listenPort := "1357"
+
+	router.Run(":" + listenPort)
+}
